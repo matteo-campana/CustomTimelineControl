acb81f7a507984621b7f096c3bb3ead84be88c31
Matteo Campana <matteo.campana996@gmail.com>
2025-03-11 16:10:41 +0100

    feat: Add WhatsApp chat service for chat retrieval

---
+++ CustomEmailTimelineControl/services/whatsappChatService.ts
@@ -0,0 +1,62 @@
+import { IInputs } from "../generated/ManifestTypes";
+import sampleData from "../sample-whatsapp-chat.json";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function extractDocumentBodyContent(base64String: string): any {
+    const decodedString = atob(base64String);
+    return JSON.parse(decodedString);
+}
+
+export async function getWhatsAppChats(context: ComponentFramework.Context<IInputs>, caseId: string | null): Promise<ComponentFramework.WebApi.Entity[]> {
+    const fetchXml = `
+        <fetch>
+            <entity name="msdyn_ocliveworkitem">
+                <attribute name="activityid" />
+                <attribute name="description" />
+                <attribute name="msdyn_channel" />
+                <attribute name="msdyn_createdon" />
+                <attribute name="msdyn_title" />
+                <attribute name="subject" />
+                ${caseId ? `
+                <filter>
+                    <condition attribute="msdyn_ocliveworkitemid" operator="eq" value="${caseId}" />
+                </filter>` : ''}
+                <order attribute="createdon" descending="true" />
+                <link-entity name="msdyn_transcript" from="msdyn_liveworkitemidid" to="activityid">
+                    <link-entity name="annotation" from="objectid" to="msdyn_transcriptid" alias="annotation">
+                        <attribute name="annotationid" />
+                        <attribute name="documentbody" />
+                        <attribute name="filename" />
+                    </link-entity>
+                </link-entity>
+            </entity>
+        </fetch>
+    `;
+
+    const query = `?fetchXml=${fetchXml}`;
+    const response = await context.webAPI.retrieveMultipleRecords("msdyn_ocliveworkitem", query);
+    if (response.entities.length > 0) {
+        console.log("WhatsApp chats retrieved:", response.entities);
+        response.entities.forEach(entity => {
+            if (entity["annotation.documentbody"]) {
+                entity["annotation.documentbody"] = extractDocumentBodyContent(entity["annotation.documentbody"]);
+            }
+        });
+        console.table(response.entities);
+        return response.entities;
+    } else {
+        return [];
+    }
+}
+
+export function getWhatsAppChatsTest(): ComponentFramework.WebApi.Entity[] {
+    const entities = sampleData.map(entity => {
+        if (entity["annotation.documentbody"]) {
+            entity["annotation.documentbody"] = extractDocumentBodyContent(entity["annotation.documentbody"]);
+        }
+        return entity;
+    });
+    console.log("WhatsApp chats (test) retrieved:", entities);
+    console.table(entities);
+    return entities;
+}
